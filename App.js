7ef92f9b14181c3ec1539d2e9645bef1efb67a7c
rickie <rcya996001@gmail.com>
2024-10-06 05:19:23 -0400

    edited

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Video } from 'expo-av';
 
-export default function App() {
+const App = () => {
+  const videoRef = React.useRef(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Video
+        ref={videoRef}
+        style={styles.video}
+        source={{
+          uri: 'https://sdo.gsfc.nasa.gov/assets/img/latest/mpeg/latest_1024_0193.mp4', // Replace with the path to your mp4 video
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        shouldPlay
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  video: {
+    width: '100%',
+    height: 300,
   },
 });
+
+export default App;
